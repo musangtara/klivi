aa7fa7b6b5d1d360fb1de2253145e6cc79b39f69
musangtara2 <80504546+musangtara@users.noreply.github.com>
2021-03-15 10:53:49 +0700

    Update camera.py

--- vm/camera.py
+++ vm/camera.py
@@ -1,11 +1,1 @@
-import sys
-
-class onject():
-    def __init__(self, name, object):
-        self.name = name
-        # HACK trick the object to be onject
-        self.object = object
-
-    def onject(self):
-        self.object = object
 
